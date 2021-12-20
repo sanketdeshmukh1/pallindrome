5bedc51b721b05f6da551fd53c6f4577fe1cec39
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2021-12-20 22:38:35 +0530

    added js

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 const dateip=document.querySelector(".ip");
 const btn=document.querySelector(".bttn");
+const ans=document.querySelector('.answer');
 
-function palinbday(){
-    console.log("hello");
+function cnvrtdate(dateipnew){
+ var strdatetemp={ day: '', month: '', year: ''};
+ if(strdatetemp.day<10){
+     strdatetemp.day='0'+strdatetemp.day;
+ }
+else{
+    strdatetemp.day=strdatetemp.day.toString();
 }
 
-btn.addEventListener("click",palinbday)
+if(strdatetemp.month<10){
+    strdatetemp.month='0'+strdatetemp.month;
+}
+else{
+   strdatetemp.month=strdatetemp.month.toString();
+}
+
+strdatetemp.year=strdatetemp.year.toString();
+
+return strdatetemp;
+}
+
+
+}
+
+function palinbday(){
+    const dateipnew=dateip.value;
+var strdate=cnvrtdate(dateipnew);
+
+    // var av=['a','b','c'];
+// var datewohyp=dateipnew.split('');
+// console.log(datewohyp);
+// var revdate=datewohyp.reverse();
+// var revstr=revdate.join('');
+// console.log(revdate);
+
+
+}
+
+btn.addEventListener("click",palinbday);
